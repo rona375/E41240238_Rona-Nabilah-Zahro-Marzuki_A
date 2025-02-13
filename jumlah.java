5bb354d990bf0308d2a561ed7fc6260538d44add
rona375 <studywrona@gmail.com>
2025-02-13 19:43:44 +0700

    Create jumlah.java

---
+++ jumlah.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author RONA NABILAH ZAHRO M
+ */
+public class jumlah {
+    public static void main(String[] args) {
+        
+        int angka1 = 1;
+        int angka2 = 2;
+        
+        int hasil = angka1 + angka2;
+        
+        System.out.println("Hasil: " + angka1 + " + " + angka2 + " = " + hasil);
+    }
+}
